00c1dbcf62cee2b1dc5d5a8bf7af9e07d2c84737
pradeepbr2003 <pradeepbr2003@gmail.com>
2025-02-27 12:11:39 +0530

    filter todos added

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,13 +1,15 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { catchError } from 'rxjs';
 import { HighlightCompletedTodoDirective } from '../directives/highlight-completed-todo.directive';
 import { Todo } from '../model/todos.type';
+import { FilterTodosPipe } from '../pipes/filter-todos.pipe';
 import { TodosService } from '../services/todos.service';
 
 @Component({
   selector: 'app-todos',
-  imports: [HighlightCompletedTodoDirective, UpperCasePipe],
+  imports: [HighlightCompletedTodoDirective, UpperCasePipe, FormsModule, FilterTodosPipe],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss'
 })
@@ -17,6 +19,8 @@ export class TodosComponent implements OnInit {
   todoService = inject(TodosService);
   todoItems = signal<Array<Todo>>([]);
   isLoaded = signal<boolean>(false);
+
+  searchTerm = signal('');
 
   todoClicked(todo: Todo) {
     todo.completed = !todo.completed;
